8c860229d4de8a24ccb8653e70c8f06624a42484
Bo-Ba <baranskikajtek@gmail.com>
2022-03-14 23:20:42 +0100

    Tournament and roulette selection, crossover, mutation

--- Genetic Algorithm - Implementation and Research/src/main/java/Main.java
+++ Genetic Algorithm - Implementation and Research/src/main/java/Main.java
@@ -1,6 +1,4 @@
-import dataClasses.Cost;
 import dataClasses.CostFlow;
-import dataClasses.Flow;
 import helpers.DataLoader;
 import helpers.PopulationHelper;
 
@@ -10,34 +8,27 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Integer[] generatedData = new Integer[24];
-        generatedData[0] = 5;
-        generatedData[1] = 7;
-        generatedData[2] = 2;
-        generatedData[3] = 4;
-        generatedData[4] = 3;
-        generatedData[5] = 0;
-        generatedData[6] = 6;
-        generatedData[7] = 1;
-        generatedData[8] = 8;
-        generatedData[9] = 9;
-        generatedData[10] = 10;
-        generatedData[11] = 11;
-        generatedData[12] = 12;
-        generatedData[13] = 13;
-        generatedData[14] = 14;
-        generatedData[15] = 15;
-        generatedData[16] = 16;
-        generatedData[17] = 17;
-        generatedData[18] = 18;
-        generatedData[19] = 19;
-        generatedData[20] = 20;
-        generatedData[21] = 21;
-        generatedData[22] = 22;
-        generatedData[23] = 30;
 
-        PopulationHelper.generatePopulation(30, 24, 5 ,6);
-        List<CostFlow> testList = DataLoader.findPairs(DataLoader.getCost("easy"), DataLoader.getFlow("easy"));
-        System.out.println(PopulationHelper.calculateCost(testList, generatedData, 5, 6));
+//        List<Integer[]> population = PopulationHelper.generatePopulation(30, 9, 3, 3);
+//        List<CostFlow> testList = DataLoader.findPairs(DataLoader.getCost("easy"), DataLoader.getFlow("easy"));
+//        System.out.println(PopulationHelper.calculateCosts(testList, population, 3, 3));
+
+        Integer[] test1 =  {
+            1, 2, 3, 4, 5,
+        };
+
+        Integer[] test2 =  {
+            6, 7, 8, 9, 10
+        };
+
+        List<Integer[]> test = PopulationHelper.singlePointCrossover(test1, test2, 3, 3);
+        System.out.println(Arrays.toString(test.get(0)));
+        System.out.println(Arrays.toString(test.get(1)));
+
+        Integer[] duplicatesList =  {
+                6, 7, 8, 9, 10, 5, 5, 5
+        };
+
+        System.out.println(Arrays.toString(PopulationHelper.fixGenotypes(duplicatesList, 9)));
     }
 }
--- Genetic Algorithm - Implementation and Research/src/main/java/helpers/PopulationHelper.java
+++ Genetic Algorithm - Implementation and Research/src/main/java/helpers/PopulationHelper.java
@@ -6,14 +6,14 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 public class PopulationHelper {
-    private static Random random;
+    private static final Random random = new Random();
 
     public static List<Integer[]> generatePopulation(int populationSize, int machinesNum, int x, int y) {
         List<Integer[]> population = new LinkedList<>();
         Integer[] locationsArray = IntStream.range(0, x * y).boxed().toArray(Integer[]::new);
 
         for (int i = 0; i < populationSize; i++) {
-            Integer[] mesh = shuffle(locationsArray);
+            Integer[] mesh = shuffle(locationsArray.clone());
 
             population.add(mesh);
         }
@@ -21,7 +21,6 @@ public class PopulationHelper {
     }
 
     private static Integer[] shuffle(Integer[] array) {
-        if (random == null) random = new Random();
         int count = array.length;
         for (int i = count; i > 1; i--) {
             swap(array, i - 1, random.nextInt(i));
@@ -35,12 +34,32 @@ public class PopulationHelper {
         array[j] = temp;
     }
 
+    private static void swapRandomIndexes(Integer[] array) {
+        Random r = new Random();
+        int firstIndex = r.nextInt(array.length);
+        int secondIndex = r.nextInt(array.length);
+        while (secondIndex == firstIndex) {
+            secondIndex = r.nextInt(array.length);
+        }
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
     public static int calculateCost(List<CostFlow> costFlows, List<Integer[]> population, int rowNum, int colNum) {
         int cost = 0;
         for (Integer[] individual : population) {
             cost += calculateCost(costFlows, individual, rowNum, colNum);
         }
         return cost;
+    }
+
+    public static List<Integer> calculateCosts(List<CostFlow> costFlows, List<Integer[]> population, int rowNum, int colNum) {
+        List<Integer> costs = new LinkedList<>();
+        for (Integer[] individual : population) {
+            costs.add(calculateCost(costFlows, individual, rowNum, colNum));
+        }
+        return costs;
     }
 
     public static int calculateCost(List<CostFlow> costFlows, Integer[] individual, int rowNum, int colNum) {
@@ -76,5 +95,74 @@ public class PopulationHelper {
         }
 
         return bestIndividual;
+    }
+
+    public static Integer[] rouletteSelection(List<CostFlow> costFlows,
+                                              List<Integer[]> population,
+                                              int tournamentSize, int rowNum,
+                                              int colNum) {
+        List<Integer> costs = calculateCosts(costFlows, population, rowNum, colNum);
+        int sum = costs.stream().mapToInt(Integer::intValue).sum();
+        List<Double> probabilities = new LinkedList<>();
+
+        double probabilitiesSum = 0;
+        for (Integer individualCost : costs) {
+            probabilitiesSum += (double) individualCost / (double) sum;
+            probabilities.add(probabilitiesSum);
+        }
+
+        Random r = new Random();
+        double randomPick = r.nextDouble();
+        for (int i = 0; i < probabilities.size(); i++) {
+            if (probabilities.get(i) < randomPick) return population.get(i);
+        }
+        return null;
+    }
+
+    public static List<Integer[]> singlePointCrossover(Integer[] firstIndividual, Integer[] secondIndividual, int meshRows, int meshColumns) {
+        int splitPoint = random.nextInt(firstIndividual.length);
+        if(splitPoint == 0) splitPoint++;
+
+        Integer[] temp = secondIndividual.clone();
+        System.arraycopy(firstIndividual, splitPoint, secondIndividual, splitPoint, firstIndividual.length - splitPoint);
+        System.arraycopy(temp, splitPoint, firstIndividual, splitPoint, firstIndividual.length - splitPoint);
+
+        fixGenotypes(firstIndividual, meshRows * meshColumns);
+        fixGenotypes(secondIndividual, meshRows * meshColumns);
+
+        return(Arrays.asList(firstIndividual, secondIndividual));
+    }
+
+    //TODO Find more optimal solution
+    public static Integer[] fixGenotypes(final Integer[] individual, int possiblePositions) {
+        HashSet<Integer> positionsSet = new HashSet<>(Arrays.asList(individual));
+        HashSet<Integer> currentSet = new HashSet<>();
+        int index = 0;
+        if(positionsSet.size() == individual.length) return individual;
+
+        for (int item : individual) {
+            if (currentSet.contains(item)) {
+                int newPosition = item;
+                HashSet<Integer> drawnNumbers = new HashSet<>();
+                while(positionsSet.contains(newPosition)) {
+                    newPosition = random.nextInt(possiblePositions);
+                    drawnNumbers.add(newPosition);
+                    if(drawnNumbers.size() == possiblePositions) break;
+                }
+                positionsSet.add(newPosition);
+                individual[index] = newPosition;
+            }
+            index++;
+            currentSet.add(item);
+        }
+
+        return individual;
+    }
+
+    public static Integer[] mutation(Integer[] individual, float mutationProbability) {
+        if (random.nextFloat() < mutationProbability) {
+            swapRandomIndexes(individual);
+        }
+        return individual;
     }
 }
